6aa58c857d6540700e028d9aa8296b1f6a5b72cc
Doreen Wiggins <Wiggy77@aol.com>
2017-12-12 22:09:11 -0500

    bamazonCustomer formatted and commented

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,23 +1,23 @@
+// npm packages needed to run app
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// create connection to mysql to store, get and manipulate information
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    // Your username
     user: "root",
-    // Your password
     password: "root",
     database: "bamazonDB"
 });
 
+// show id of successful connection or if error connecting throw error 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 });
 
-// var allProducts = [];
-
+// function to show all products sold along with their item number and price per the mysql products database
 function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         console.log("Welcome to Bamazon!");
@@ -30,15 +30,11 @@ function queryAllProducts() {
             console.log("Price: $" + (res[i].price).toFixed(2));
             console.log("-----------------------------------");
         }
-        // console.log(allProducts);
-        // return allProducts;
     });
-
 };
 
 
-// console.log(allProducts);
-
+// function to walk customer through purchasing an item by prompting for item number and quantity
 function buyItem() {
     inquirer
         .prompt([{
@@ -52,18 +48,20 @@ function buyItem() {
                 name: "quantity"
             },
         ])
+
+        // takes customer order input and compares to database
+
         .then(function (answer) {
-            // console.log(answer.productid);
             connection.query("SELECT * FROM products", function (err, res) {
 
+                // loops through product table to see if an item id matches the user input
+
                 for (var i = 0; i < res.length; i++) {
-                    // console.log(res[i]);
                     if (res[i].item_id === parseInt(answer.productid)) {
                         var itemMatch = 1;
-                        console.log("It worked!");
-                        // console.log(res[i]);
-                        // console.log(res[i].stock_quantity);
-                        // console.log(parseInt(answer.quantity));
+
+                        // checks to see if there is enough inventory to fulfill customer order
+
                         if ((res[i].stock_quantity - parseInt(answer.quantity) >= 0)) {
                             var itemsLeft = res[i].stock_quantity - parseInt(answer.quantity);
                             var totalCost = parseInt(answer.quantity) * res[i].price;
@@ -72,6 +70,9 @@ function buyItem() {
                             console.log("Your order total is  $" + totalCost.toFixed(2) + ".");
                             console.log(itemsLeft);
                             console.log(productId);
+
+                            // updates resulting inventory in products database
+
                             connection.query(
                                 "UPDATE products SET ? WHERE ?", [{
                                         stock_quantity: itemsLeft
@@ -80,16 +81,26 @@ function buyItem() {
                                         item_id: productId
                                     }
                                 ]);
+
+                            // alers customer if not enough in stock
+
                         } else {
                             console.log("Insufficient quantity available to fill this order.");
                         }
                     }
                 };
+
+                // alerts customer if item number entered is not found
+
                 if (itemMatch != 1) {
                     console.log("Item not found.");
                 };
             });
         });
 };
+
+// calls queryAllProducts to list items automatically when program is run
 queryAllProducts();
+
+// calls buyItem to run after a half second delay to prevent it from running before the AllProducts list is shown
 setTimeout(buyItem, 500);
